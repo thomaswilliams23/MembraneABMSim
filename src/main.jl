3442ef0acb285e3570787e27bcd704589419b8f4
thomaswilliams23 <thomaswilliamsmusic@gmail.com>
2025-12-03 11:44:41 +1100

    potential bugfix

--- src/main.jl
+++ src/main.jl
@@ -130,6 +130,8 @@ function run_sim(config_pathname::String; clear_existing_output::Bool=false)
         #if any nascent agents have been promoted, need to update flat data structure on gpu
         if device=="metal" && grid_size.nascent_promoted
             update_nascent_promotion_metal!(all_data_metal, system_flat_cpu, grid_size)
+        elseif device=="cuda" && grid_size.nascent_promoted
+            update_nascent_promotion_cuda!(all_data_CUDA, system_flat_cpu, grid_size)
         end
 
         #check for any new tethering or assembly
