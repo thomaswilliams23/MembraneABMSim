bb2d362fec341b5065296e2d15fe58a8689822a0
thomaswilliams23 <thomaswilliamsmusic@gmail.com>
2025-12-03 12:20:51 +1100

    more debugging

--- src/biology.jl
+++ src/biology.jl
@@ -236,6 +236,7 @@ function update_BAM_subsystem!(agents::AllAgents, grid_size::GridSize, grid::Sim
                 #now update identifiers of all nascent agents and their inserting agents (since their nascent_ix has changed)
                 if length(nascent_OMP_ixs_to_delete)>0
 
+                    #DEBUG
                     println("Promoted $(length(nascent_OMP_ixs_to_delete)) nascent OMP agents at time t=$(t)")
 
                     update_flat_data_nascent_agents!(agents, system_flat)
@@ -488,6 +489,12 @@ function update_Lpt_subsystem!(agents::AllAgents, grid_size::GridSize, system_fl
 
                 #now update identifiers of all nascent agents and their inserting agents (since their nascent_ix has changed)
                 if length(nascent_LPS_ixs_to_delete)>0
+
+                    #DEBUG
+                    println("Promoted $(length(nascent_LPS_ixs_to_delete)) nascent LPS agents at time t=$(t)")
+                    println("Total nascent LPS remaining: $(length(agents.nascent.nascent_LPS))")
+
+
                     update_flat_data_nascent_agents!(agents, system_flat)
                     grid_size.nascent_promoted = true
                 end
